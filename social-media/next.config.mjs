a656fafc60141cd91e432710c632fc8a2fe99268
assoly-lab <81777632+assoly-lab@users.noreply.github.com>
2024-11-13 22:10:26 +0100

    removed deprecated domains configuration

--- social-media/next.config.mjs
+++ social-media/next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'social-media-frontend-woad.vercel',
-            pathname: '/media/**/**',
-          },
-        ],
-        domains: ['social-media-frontend-woad.vercel.app'],
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.vercel.app",
       },
+    ],
+    domains: ["social-media-frontend-woad.vercel.app"],
+  },
 };
 
 export default nextConfig;
